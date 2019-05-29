4810df23a3133effab4caaf77272ad04fb882bae
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 05:37:26 +0700

    struktur awal aplikasi bbta3 mob

---
+++ android/app/src/main/java/com/bbta3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bbta3;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "bbta3";
+    }
+}
