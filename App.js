54963cab9b16d5a935eca1b65f1874d65a07e2c7
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 06:20:20 +0700

    melakukan optimisasi penggunaan screen untuk react-navigation

--- App.js
+++ App.js
@@ -8,6 +8,9 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { useScreens } from 'react-native-screens';
+
+useScreens();
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
--- android/app/src/main/java/com/bbta3/MainActivity.java
+++ android/app/src/main/java/com/bbta3/MainActivity.java
@@ -1,11 +1,16 @@
 package com.bbta3;
 
-import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle;
+import com.facebook.react.ReactFragmentActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
