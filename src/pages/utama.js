f555ce904d48cd3d0a4ec82a4cf9ee62c16debd1
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 10:50:06 +0700

    membuat halaman utama

---
+++ src/pages/utama.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+class Utama extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
+    }
+}
+
+export default Utama
