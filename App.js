5d7af6b178b2f261c83d194e98a91753ffb1073c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 10:49:32 +0700

    penyederhanaan main app

--- App.js
+++ App.js
@@ -1,58 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import {useScreens} from 'react-native-screens'
 
-import React, {Component} from 'react';
-import { 
-  Container, 
-  Header, 
-  Title, 
-  Content, 
-  Footer, 
-  FooterTab, 
-  Button,
-  Body,
-  Text,
-  Icon
-} from 'native-base'
-import { useScreens } from 'react-native-screens';
+import Routes from './src/routes'
 
-useScreens();
+useScreens()
 
-export default class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Header noLeft>
-          <Body>
-            <Title>BBTA3 BPPT</Title>
-          </Body>
-        </Header>
-
-        <Content>
-          <Text>
-            This is Content Section
-          </Text>
-        </Content>
-
-        <Footer>
-          <FooterTab>
-            <Button active>
-              <Icon active name="home" />
-            </Button>
-            <Button>
-              <Icon name="book" />
-            </Button>
-            <Button>
-              <Icon name="person" />
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    )
-  }
-}
+export default Routes
