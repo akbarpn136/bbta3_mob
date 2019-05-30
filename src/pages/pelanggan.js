3fc5706514e0b2c4e4b5ba4437be8c09e781505d
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 10:49:56 +0700

    membuat halaman pelanggan

---
+++ src/pages/pelanggan.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+class Pelanggan extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Pelanggan!</Text>
+            </View>
+        );
+    }
+}
+
+export default Pelanggan
