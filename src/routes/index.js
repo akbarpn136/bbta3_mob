84d60215a6c23c1d6797eb242aa18b932ae67269
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-05-30 12:06:23 +0700

    melakukan custom footer tab bar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
+import React from 'react'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 
 import UtamaPage from '../pages/utama'
 import InfoPage from '../pages/info'
 import PelangganPage from '../pages/pelanggan'
+import BottomBar from '../components/bottomBar'
 
-const TabNavigator = createBottomTabNavigator({
-  Utama: UtamaPage,
-  Info: InfoPage,
-  Pelanggan: PelangganPage
+const TabNav = createBottomTabNavigator({
+    Utama: UtamaPage,
+    Info: InfoPage,
+    Pelanggan: PelangganPage
+}, {
+    tabBarComponent: props => <BottomBar {...props} />
 })
 
-export default createAppContainer(TabNavigator)
+export default createAppContainer(TabNav)
