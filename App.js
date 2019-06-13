aee6906151739b9739b6898e1213bd6f8fea0dec
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-13 16:46:54 +0700

    melakukan konfigurasi rinci Komponen App Provider

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
+import React from 'react'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import {useScreens} from 'react-native-screens'
 
 import Routes from './src/routes'
+import reducers from './src/stores/reducers'
 
 useScreens()
 
-export default Routes
+export default class App extends React.Component {
+    render() {
+        return (
+            <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+                <Routes />
+            </Provider>
+        )
+    }
+}
