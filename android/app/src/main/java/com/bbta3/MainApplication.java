cec555f64e224120136a96764ec4305fe75fa2ff
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-17 16:27:57 +0700

    melakukan linking asset webview ke platform android ios

--- android/app/src/main/java/com/bbta3/MainApplication.java
+++ android/app/src/main/java/com/bbta3/MainApplication.java
@@ -3,6 +3,7 @@ package com.bbta3;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCWebViewPackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage()
       );
