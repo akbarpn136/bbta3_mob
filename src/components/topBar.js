328dbd2a313d44d9a8ad318322d0d23d7140329f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-17 16:28:55 +0700

    membuat komponen baru untuk Top Bar

---
+++ src/components/topBar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+    Header,
+    Body,
+    Title,
+    Left,
+    Right,
+    Button,
+    Icon
+} from 'native-base'
+
+class ParentStacked extends React.Component {
+    render() {
+        return (
+            <Header noLeft>
+                <Body>
+                    <Title>{this.props.title ? this.props.title : 'BBTA3 BPPT'}</Title>
+                </Body>
+            </Header>
+        )
+    }
+}
+
+class ChildStacked extends React.Component {
+    render() {
+        return (
+            <Header>
+                <Left>
+                    <Button transparent 
+                        onPress={() => this.props.push.goBack()}>
+                        <Icon name="arrow-back" />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>{this.props.title ? this.props.title : 'BBTA3 BPPT'}</Title>
+                </Body>
+                <Right />
+            </Header>
+        )
+    }
+}
+
+class TopBar extends React.Component {
+    render() {
+        if (this.props.childStacked) return (<ChildStacked push={this.props.push} />)
+        else return (<ParentStacked />)
+    }
+}
+
+export default TopBar
