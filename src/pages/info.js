b8b0a8fbb1482536526a600a147d9ff5efd86ad4
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-20 14:26:55 +0700

    menerapkan flatlist dan fungsi push nav berita

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import { FlatList } from 'react-native'
+import {connect} from 'react-redux'
+import {FlatList} from 'react-native'
 
 import ListBerita from '../components/listBerita'
 import TopBar from '../components/topBar'
+import {fetchBerita} from '../stores/actions/beritaAction'
 
 class Info extends React.Component {
+    componentDidMount() {
+        this.props.fetchBerita(1)
+    }
+
     static navigationOptions = {
         header: <TopBar title={'INFO BBTA3'} />,
     }
 
     render() {
         return (
-            <FlatList data={[{key: 'a'}, {key: 'b'}]}
-                renderItem={({item}) => <ListBerita />} />
-        );
+            <FlatList data={this.props.berita}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({item}) => <ListBerita data={item} push={this.props.navigation} />} />
+        )
     }
 }
 
-export default Info
+const mapStateToProps = state => {
+    return {
+        berita: state.berita
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchBerita: (id) => dispatch(fetchBerita(id)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Info)
