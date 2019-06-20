639ee6c3c7a3fe735ec1cd685d9bf25a5c663a5a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-20 10:17:31 +0700

    melakukan integrasi berita ke root reducer

--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
 
 import {keahlianReducer} from './keahlian/keahlianReducer'
+import {beritaReducer} from './info/beritaReducer'
 
 export default combineReducers({
-    keahlian: keahlianReducer
+    keahlian: keahlianReducer,
+    berita: beritaReducer
 })
