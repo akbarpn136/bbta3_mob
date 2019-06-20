6164dcdc1f486d59d1485382ad006975609afc7c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-20 14:29:11 +0700

    menambahkan page rincian berita kedalam stack nav

--- src/routes/info/index.js
+++ src/routes/info/index.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from 'react-navigation'
 
 import BeritaPage from '../../pages/info'
+import RincianPage from '../../pages/includes/rincianBerita'
 
 export const InfoNavigator = createStackNavigator(
     {
         Berita: BeritaPage,
+        RincianBerita: RincianPage
     }
 )
