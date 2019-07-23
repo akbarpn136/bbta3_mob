d6d65d3abd04631c85fcd8880ef60c86ecd2a6b9
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-23 11:08:30 +0700

    ganti struktur aplikasi mobile bbta3

--- src/stores/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import {keahlianReducer} from './keahlian/keahlianReducer'
-import {beritaReducer} from './info/beritaReducer'
-
-export default combineReducers({
-    keahlian: keahlianReducer,
-    berita: beritaReducer
-})
