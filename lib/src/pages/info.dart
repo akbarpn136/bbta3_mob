b4880316dd3721c1ef6d00883e3c776ddc95ed54
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-26 15:15:38 +0700

    membuat halaman awal untuk informasi dan pelanggan

---
+++ lib/src/pages/info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Info extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text('HALAMAN INFORMASI'),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/pelanggan.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Pelanggan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text('HALAMAN PELANGGAN'),
+      ),
+    );
+  }
+}
