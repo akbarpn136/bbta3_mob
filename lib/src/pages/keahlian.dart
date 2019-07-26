8208e28495f153cc19eb2814616b651d8f4c4a5a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-26 15:16:04 +0700

    penyederhanaan tata letak awal halaman keahlian

--- lib/src/pages/keahlian.dart
+++ lib/src/pages/keahlian.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:bbta3/src/components/navbar.dart';
-
 
 class Keahlian extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Navbar.make('BBTA3 BPPT'),
-      body: SafeArea(
-        child: Center(
-          child: Text('HALAMAN KEAHLIAN'),
-        ),
+    return SafeArea(
+      child: Center(
+        child: Text('HALAMAN KEAHLIAN'),
       ),
     );
   }
