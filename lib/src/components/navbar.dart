7a08f5f5fbefae28103b8bd6952ec43aa5a70508
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-26 15:14:40 +0700

    atur tata letak logo dan text style

--- lib/src/components/navbar.dart
+++ lib/src/components/navbar.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:bbta3/src/helpers/styles.dart';
+
 
 class Navbar {
   static AppBar make(String title) {
     return AppBar(
-      elevation: 0.0,
+      elevation: 0.5,
+      leading: Container(
+        margin: EdgeInsets.only(
+          top: 10.0,
+          left: 10.0,
+          bottom: 10.0
+        ),
+        child: Image.asset(
+          'images/bbta3.png',
+          alignment: Alignment.center,
+        ),
+      ),
       title: Text(
         title,
+        style: HeaderNavBar,
       ),
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.red.shade600,
     );
   }
 }
