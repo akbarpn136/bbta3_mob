d3f88e329a30f98c08021e085e07d9c446ac9571
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-28 22:28:38 +0700

    membuat Text Style widget pendukung

--- lib/src/helpers/styles.dart
+++ lib/src/helpers/styles.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 const TextStyle HeaderNavBar = TextStyle(
   fontFamily: 'AlfaSlabOne',
 );
+
+const TextStyle KeahlianTitle = TextStyle(
+  fontWeight: FontWeight.w400,
+  fontSize: 28.0,
+);
+
+const TextStyle KeahlianTitleOverflow = TextStyle(
+  fontFamily: 'AlfaSlabOne',
+  fontWeight: FontWeight.w400,
+  fontSize: 28.0,
+  color: Colors.white,
+);
