ce81238d3ebee72d83def427c81b9a62e3ebcbfa
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-30 15:55:13 +0700

    melakuka perubahan jenis widget yang digunakan

--- lib/src/components/navbar.dart
+++ lib/src/components/navbar.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 import 'package:bbta3/src/helpers/styles.dart';
 
-
 class Navbar {
-  static AppBar make(String title) {
+  static AppBar make(String title, Function onPress) {
     return AppBar(
       elevation: 0.5,
       leading: Container(
         margin: EdgeInsets.only(
           top: 10.0,
           left: 10.0,
-          bottom: 10.0
+          bottom: 10.0,
         ),
         child: Image.asset(
           'images/bbta3.png',
@@ -23,6 +23,15 @@ class Navbar {
         style: HeaderNavBar,
       ),
       backgroundColor: Colors.red.shade600,
+      actions: <Widget>[
+        Container(
+          child: IconButton(
+            icon: Icon(EvaIcons.questionMarkCircleOutline),
+            color: Colors.white,
+            onPressed: onPress,
+          ),
+        ),
+      ],
     );
   }
 }
