7a7f5c3fdd9f162bc402fcc69a9d45a37909d6f9
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-01 10:30:55 +0700

    ganti icon yang digunakan serta integrasi app browser

--- lib/src/components/bottom_tentang.dart
+++ lib/src/components/bottom_tentang.dart
@@ -1,7 +1,20 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BottomTentang extends StatelessWidget {
+  final ChromeSafariBrowser chromeSafariBrowser =
+      ChromeSafariBrowser(InAppBrowser());
+
+  Future<void> visitSite(String link) async {
+    await chromeSafariBrowser.open(link, options: {
+      "addShareButton": true,
+      "toolbarBackgroundColor": "#FFE53935",
+      "dismissButtonStyle": 1,
+      "preferredBarTintColor": "#000000",
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,24 +25,103 @@ class BottomTentang extends StatelessWidget {
           Row(
             children: <Widget>[
               Image.asset('images/bbta3.png'),
-              SizedBox(width: 11.0,),
+              SizedBox(
+                width: 11.0,
+              ),
               Image.asset('images/bppt.png'),
             ],
           ),
-          SizedBox(height: 21.0,),
-          Text(
-            'BBTA3 (Balai Besar Teknologi Aerodinamika, Aeroelastika dan Aeroakustika) merupakan transformasi dari unit kerja lama UPT LAGG dengan konsep baru dengan cakupan keahlian yang lebih luas.'
+          SizedBox(
+            height: 21.0,
           ),
-          SizedBox(height: 21.0,),
+          Text(
+              'BBTA3 (Balai Besar Teknologi Aerodinamika, Aeroelastika dan Aeroakustika) merupakan transformasi dari unit kerja lama UPT LAGG dengan konsep baru dengan cakupan keahlian yang lebih luas.'),
+          SizedBox(
+            height: 21.0,
+          ),
+          Text(
+              'Kawasan PUSPIPTEK, Gd. 240, Setu, Tangerang Selatan – Banten (15314)'),
+          SizedBox(
+            height: 21.0,
+          ),
           Row(
             children: <Widget>[
-              FlatButton(
-                child: Icon(EvaIcons.facebook),
-                onPressed: () {},
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Color(0xff3b5998),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.facebookF,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://www.facebook.com/BBTA3/'),
+                ),
               ),
-              FlatButton(
-                child: Icon(EvaIcons.twitter),
-                onPressed: () {},
+              SizedBox(width: 8.0),
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Color(0xffE1306C),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.instagram,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://www.instagram.com/bbta3.bppt/'),
+                ),
+              ),
+              SizedBox(width: 8.0),
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Color(0xff55acee),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.twitter,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://twitter.com/@bbta3_bppt'),
+                ),
+              ),
+              SizedBox(width: 8.0),
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Color(0xffe02a20),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.youtube,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://www.youtube.com/channel/UCa0_hm_SiHxps1Llk_q6I1Q'),
+                ),
+              ),
+              SizedBox(width: 8.0),
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Colors.black54,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.slack,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://github.com/akbarpn136'),
+                ),
+              ),
+              SizedBox(width: 8.0),
+              ButtonTheme(
+                minWidth: 55.0,
+                buttonColor: Colors.black,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  child: Icon(
+                    FontAwesomeIcons.github,
+                    size: 18.0,
+                  ),
+                  onPressed: () => visitSite('https://github.com/akbarpn136'),
+                ),
               ),
             ],
           ),
