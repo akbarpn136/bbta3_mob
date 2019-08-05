02da7f23a530db6265f484224aee0e8636d0ca84
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-05 21:32:30 +0700

    membuat kondisi penggunaan widget terkait http

--- lib/src/pages/keahlian.dart
+++ lib/src/pages/keahlian.dart
@@ -23,6 +23,7 @@ class _KeahlianState extends State<Keahlian> {
   }
 
   void buildCardKeahlian() async {
+    Widget ahliCard;
     Ahli keahlian = Ahli();
     List<dynamic> dataKeahlian = await keahlian.readKeahlian();
     ChromeSafariBrowser chromeSafariBrowser =
@@ -30,58 +31,86 @@ class _KeahlianState extends State<Keahlian> {
     isLoading = false;
 
     for (Map<String, dynamic> ahli in dataKeahlian) {
-      String namaKeahlian = ahli['title']['rendered'];
-      String imageURL = ahli['_embedded']['wp:featuredmedia'][0]['source_url'];
-      String link = ahli['link'];
-      Widget ahliCard = InkWell(
-        borderRadius: BorderRadius.circular(10.0),
-        highlightColor: Colors.blue.shade100,
-        onTap: () async {
-          await chromeSafariBrowser.open(link, options: {
-            "addShareButton": true,
-            "toolbarBackgroundColor": "#FFE53935",
-            "dismissButtonStyle": 1,
-            "preferredBarTintColor": "#000000",
-          });
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(11.5),
-            child: Stack(
-              children: <Widget>[
-                Image.network(
-                  imageURL,
-                  fit: BoxFit.cover,
-                  height: 1000.0,
+      if (ahli.containsKey('error')) {
+        ahliCard = Container(
+          alignment: AlignmentDirectional.center,
+          child: Container(
+            padding: EdgeInsets.all(19.0),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              border: Border(
+                top: BorderSide(
+                  color: Colors.red.shade300,
+                  width: 3.0,
                 ),
-                Positioned(
-                  bottom: 0.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromARGB(200, 0, 0, 0),
-                          Color.fromARGB(0, 0, 0, 0)
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                    child: Text(
-                      namaKeahlian,
-                      style: KeahlianTitleOverflow,
-                    ),
-                  ),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black38,
+                  offset: Offset(0.0, 3.0),
+                  blurRadius: 1.5,
                 ),
               ],
             ),
+            child: Text(ahli['error']),
           ),
-        ),
-      );
+        );
+      } else {
+        String namaKeahlian = ahli['title']['rendered'];
+        String imageURL =
+            ahli['_embedded']['wp:featuredmedia'][0]['source_url'];
+        String link = ahli['link'];
+        ahliCard = InkWell(
+          borderRadius: BorderRadius.circular(10.0),
+          highlightColor: Colors.blue.shade100,
+          onTap: () async {
+            await chromeSafariBrowser.open(link, options: {
+              "addShareButton": true,
+              "toolbarBackgroundColor": "#FFE53935",
+              "dismissButtonStyle": 1,
+              "preferredBarTintColor": "#000000",
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(11.5),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(
+                    imageURL,
+                    fit: BoxFit.cover,
+                    height: 1000.0,
+                  ),
+                  Positioned(
+                    bottom: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0)
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        namaKeahlian,
+                        style: KeahlianTitleOverflow,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }
 
       setState(() {
         keahlianCard.add(ahliCard);
