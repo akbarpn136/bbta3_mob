613079a977d64bd3d7e7c2d893a0c381e66a472c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-05 15:25:49 +0700

    tambah style untuk code version app

--- lib/src/helpers/styles.dart
+++ lib/src/helpers/styles.dart
@@ -9,6 +9,13 @@ const TextStyle KeahlianTitle = TextStyle(
   fontSize: 28.0,
 );
 
+const TextStyle versiTitle = TextStyle(
+  fontSize: 12.0,
+  color: Colors.grey,
+  fontWeight: FontWeight.w400,
+  fontStyle: FontStyle.italic,
+);
+
 const TextStyle KeahlianTitleOverflow = TextStyle(
   fontFamily: 'AlfaSlabOne',
   fontWeight: FontWeight.w400,
