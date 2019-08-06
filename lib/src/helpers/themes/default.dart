88f8a07a3f2eb775cec670086ba0f9b086106dad
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-06 10:36:34 +0700

    membuat theme default

---
+++ lib/src/helpers/themes/default.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:bbta3/src/helpers/variables.dart';
+
+const TextTheme DEFAULT_TEXT_THEME = TextTheme(
+  body1: TextStyle(
+    fontFamily: 'FiraSans',
+    color: Color(BLACK),
+  ),
+);
+
+const TextTheme DEFAULT_PRIMARY_TEXT_THEME = TextTheme(
+  title: TextStyle(
+    fontFamily: 'AlfaSlabOne',
+  ),
+);
