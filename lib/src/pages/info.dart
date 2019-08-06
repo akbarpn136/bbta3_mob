566317a197d1e9f9d0b65a27f08e14b360157f53
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-06 10:37:12 +0700

    melakukan perombakan tata letak halaman informasi

--- lib/src/pages/info.dart
+++ lib/src/pages/info.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:bbta3/src/models/berita.dart';
+import 'package:bbta3/src/components/item_berita.dart';
+import 'package:bbta3/src/helpers/styles.dart';
+
 class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Image.asset(
-            'images/underconstruction.png',
-            width: double.infinity,
-          )
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Berita BBTA3',
+              style: HeaderBeritaStyle,
+            ),
+          ),
         ],
       ),
     );
