76fc771b6f35219cb8308312158ff7afd188f769
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-06 14:45:51 +0700

    menambahkan variabel color baru

--- lib/src/helpers/variables.dart
+++ lib/src/helpers/variables.dart
@@ -1,5 +1,6 @@
 const int BLACK = 0xFF595757;
 const int RED = 0xFFF22E2E;
 const int BLUE = 0xFF11A0D9;
-const int YELLOW = 0xFFF2CB05;
+const int YELLOW = 0xFFECD078;
 const int ORANGE = 0xFFF29F05;
+const int GREEN = 0xFF53777A;
