e35885850ba70de9fbe1a8179c646489f4e97acd
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-09 15:39:05 +0700

    modifikasi lanjut halaman informasi BBTA3 mob

--- lib/src/pages/info.dart
+++ lib/src/pages/info.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'package:bbta3/src/models/berita.dart';
-import 'package:bbta3/src/components/item_berita.dart';
-import 'package:bbta3/src/helpers/styles.dart';
 
-class Info extends StatelessWidget {
+import 'package:bbta3/src/components/item_berita.dart';
+import 'package:bbta3/src/components/bbta3_card.dart';
+import 'package:bbta3/src/components/build_spinner.dart';
+
+import 'package:bbta3/src/helpers/styles.dart';
+import 'package:bbta3/src/helpers/variables.dart';
+
+import 'package:bbta3/src/services/berita_service.dart';
+
+class Info extends StatefulWidget {
+  @override
+  _InfoState createState() => _InfoState();
+}
+
+class _InfoState extends State<Info> {
+  bool isLoading;
+  List<dynamic> koleksiBerita;
+  int jumlahBerita;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isLoading = true;
+    jumlahBerita = 0;
+
+    buildBerita();
+  }
+
+  void buildBerita() async {
+    BeritaService beritaService = BeritaService();
+
+    koleksiBerita = await beritaService.readBerita();
+
+    if (koleksiBerita.first.containsKey('error')) {
+      setState(() {
+        isLoading = false;
+        jumlahBerita = -1;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+        jumlahBerita = koleksiBerita.length;
+      });
+    }
+  }
+
+  Widget emptyOrErrorWidget() {
+    DateFormat formatter = DateFormat('dd-MM-yyyy');
+
+    if (jumlahBerita == 0) {
+      return isLoading ? BuildSpinner() : Container(
+        margin: EdgeInsets.only(bottom: 15.0, left: 8.0, right: 8.0),
+        child: BBTA3Card(
+          cardColor: Color(BLUE),
+          cardChild: Text('Data masih kosong'),
+        ),
+      );
+    } else if (jumlahBerita == -1) {
+      return isLoading ? BuildSpinner() : Container(
+        margin: EdgeInsets.only(bottom: 15.0, left: 8.0, right: 8.0),
+        child: BBTA3Card(
+          cardColor: Color(RED),
+          cardChild: Text(koleksiBerita.first['error']),
+        ),
+      );
+    } else {
+      return isLoading ? BuildSpinner() : Expanded(
+        child: ListView.builder(
+          itemCount: jumlahBerita,
+          itemBuilder: (BuildContext context, int index) {
+            DateTime dateTime = DateTime.parse(koleksiBerita[index]['date']);
+            return ItemBerita(
+              berita: Berita(
+                judul: koleksiBerita[index]['title']['rendered'],
+                diterbitkan: formatter.format(dateTime),
+              ),
+            );
+          },
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -18,19 +100,7 @@ class Info extends StatelessWidget {
               style: HeaderBeritaStyle,
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return ItemBerita(
-                  berita: Berita(
-                    judul: 'Judul Berita #$index',
-                    diterbitkan: '20 Mei 2019',
-                  ),
-                );
-              },
-            ),
-          ),
+          emptyOrErrorWidget(),
         ],
       ),
     );
