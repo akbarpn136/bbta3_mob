5078cb818cc4cd47437c51caee1ec0f7362b30cb
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-09 15:39:41 +0700

    membuat service http baru untuk berita

---
+++ lib/src/services/berita_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+
+const BERITA_URL = 'http://bbta3.bppt.go.id/wp-json/wp/v2/posts/';
+
+class BeritaService {
+  Dio dio = new Dio();
+  int page;
+
+  BeritaService({this.page = 1});
+
+  Future<List<dynamic>> readBerita() async {
+    try {
+      var response = await dio.get(BERITA_URL, queryParameters: {
+        "_embed": true,
+        "page": page,
+      });
+
+      return response.data;
+    } catch (err) {
+
+      return [{
+        "error": err.error.message,
+      }];
+    }
+  }
+}
